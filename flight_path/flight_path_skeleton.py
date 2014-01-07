59591eeab6143eb7739d8c71cc74eb15d5a6bbee
Brian Schuster <brian.daniel.schuster@gmail.com>
2014-01-06 22:48:41 -0500

    Started Flight Path Skeleton

--- flight_path/flight_path_skeleton.py
+++ flight_path/flight_path_skeleton.py
@@ -1,6 +1,26 @@
 def flight_path_skeleton(flight, no_fly):
-	flightid = flight[0]
-	current = flight[1]
 	destination = flight[2]
+	next_start = flight[1]
 	
+	skeleton = [flight[1]]	
+	
+	while skeleton[-1] != destination:
+		next_line = shapely.geometry.LineString(no_fly, [next_start, destination])
+		intersect_test = test_intersect(no_fly, next_line):
+			if False:
+				skeleton.append(destination)
+			else:
+				next_start = avoidance(next_start, destination, intersect_test)
+		
+		
 	return skeleton ## List of ordinals that will accurately avoid no_fly zones
+	
+
+def test_intersect(no_fly, line):
+	for zone in no_fly:
+		if line.intersects(zone[0]) == True:
+			return zone
+	return False
+	
+def avoidance(curr_point, dest_point, zone):
+	
--- flight_path/no_fly_zone_chart.py
+++ flight_path/no_fly_zone_chart.py
@@ -1,11 +1,17 @@
 # coding: utf-8
-import numpy as np
-import matplotlib.pyplot as plt
-import shapely.geometry
-import descartes
 import csv
+import numpy as np
+import random
+import os
+import math
+import shapely.geometry
+import shapely.ops
 import itertools
 
+
+### Files to Execute
+execfile('open_rz_csv.py')
+execfile('flight_path.skeleton.py')
 execfile('open_rz_csv.py')
 
 file = 'restrictedZones.csv'
